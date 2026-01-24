f6a710019e5381fdeb9d9174bceb9298f8daabd0
Artyom Ulanchik <artyom.u@outlook.com>
2026-01-24 21:13:08 +0200

    updated env files

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,16 @@
 import dotenv from 'dotenv'
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const config = {
-    port: process.env.PORT || 3000,
-    api: process.env.OPENALEX_API!
+    nodeEnv: process.env.NODE_ENV || 'development', isProduction,
+    port: parseInt(process.env.PORT || '3000', 10),
+    api: process.env.OPENALEX_API!,
+    logLevel: isProduction ? 'warn' : 'info',
+    enableMorgan: !isProduction,
+};
+
+if (!config.api) {
+    throw new Error('OPENALEX API key is missing.');
 }
