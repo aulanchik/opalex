8a7dd479018b8b11f64a0e88652d12950fb4eeb9
Artyom Ulanchik <artyom.u@outlook.com>
2026-01-24 21:13:56 +0200

    overall improvements

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,37 @@ import app from '@/app';
 import { config } from '@/config/env';
 import { Logger } from '@/utils/logger';
 
-app.listen(config.port, () => {
+const server = app.listen(config.port, () => {
     Logger.success(`Server is up at port ${config.port}`)
 })
+
+const shutdown = (signal: string) => {
+    Logger.info(`Received ${signal}. Shutting it down gracefully...`);
+    server.close((err) => {
+        if (err) {
+            Logger.error(`Error occured while shutting down: ${err.message}`);
+            process.exit(1);
+        }
+        Logger.success(`Server shut down successfully!`);
+        process.exit(0);
+    })
+}
+
+const signals = ['SIGTERM', 'SIGINT'];
+
+signals.forEach(signal => process.on(signal, () => shutdown(signal)));
+
+process.on('uncaughtException', (err) => {
+    Logger.error(`Uncaught Exception: ${err.message}`);
+    process.exit(1);
+});
+
+process.on('uncaughtException', (err) => {
+    Logger.error(`Uncaught Exception: ${err.message}`);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    Logger.error(`Unhandled Rejection at ${promise}: ${reason}`);
+    process.exit(1);
+});
